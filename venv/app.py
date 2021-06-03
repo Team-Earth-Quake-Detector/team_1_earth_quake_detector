e719a4065549372c432601fd8d18e2489081c329
Alicia <aliciamarleen@gmx.de>
2021-06-03 16:15:22 +0200

    Added flask code to initiate webserver

---
+++ venv/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__)
+
+picFolder = os.path.join('static', 'images')
+print(picFolder)
+app.config['UPLOAD_FOLDER'] = picFolder
+
+
+@app.route("/")
+def index():
+    logo_hsd = os.path.join(app.config['UPLOAD_FOLDER'], 'HSD_Logo.png')
+    return render_template("webserver.html", user_image=logo_hsd)
+
+
+@app.route("/index")
+def hello():
+    return "Hello"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
