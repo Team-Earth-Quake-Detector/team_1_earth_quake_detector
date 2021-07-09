4b8a81aec326f7be090b24b96fc0f16895f99172
Gabriel Sacher <83067082+Codeinator-0711@users.noreply.github.com>
2021-07-09 14:28:48 +0200

    Updated Readme.md

--- use_case_code/app.py
+++ use_case_code/app.py
@@ -14,8 +14,8 @@ def index():
     my_map.save_map(os.path.join(app.root_path, "templates", "my_map.html"))
     #my_map = input.build_map(coordinates=(34.052234, -118.243685), radius=250)
     #my_map.save_map(os.path.join(app.root_path, "templates", "my_map.html"))
-    return render_template("index.html") # render html
-    # return my_map._repr_html_() # Extract map only -> Beautiful Soap
+    return render_template("index.html", location="TBD", radius="TBD",) # render html
+
 
 @app.route('/map')
 def map():
