66cfe1a65f792ac787dc300daeb9abc1688a70f0
karinahasler <karina.hasler@study.hs-duesseldorf.de>
2021-08-09 12:19:51 +0200

    Implemented place of strongest earthquake functionality

--- use_case_code/monitor.py
+++ use_case_code/monitor.py
@@ -75,5 +75,5 @@ class Monitor:
         place_of_strongest_worldwide = earthquake_analytics.get_place_of_strongest_earthquake_worldwide()
 
         return total_filtered, minor_filtered, moderate_filtered, strong_filtered, closest_filtered, place_of_closest_filtered,\
-               strongest_filtered, total_worldwide, minor_worldwide, moderate_worldwide, strong_worldwide, strongest_worldwide,\
+               strongest_filtered, total_worldwide, minor_worldwide, moderate_worldwide, strong_worldwide, strongest_worldwide, \
                place_of_strongest_worldwide
